55a682ec7a672571c9c937ef34ef0a200b854503
jaeyumn <wkddkgodsla@gmail.com>
2024-01-10 20:48:22 +0900

    [setting] CORS 설정

--- src/main/java/com/playground/global/config/SecurityConfig.java
+++ src/main/java/com/playground/global/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.playground.global.config;
 
 import com.playground.global.security.Role;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,15 +13,25 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    @Value("${cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+
+                .cors(Customizer.withDefaults())
 
                 .headers(httpSecurityHeadersConfigurer ->
                         httpSecurityHeadersConfigurer
@@ -32,12 +45,27 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
                                 .requestMatchers("/members/sign-up").permitAll()
-                                .requestMatchers("/members/login").permitAll()
-                                .requestMatchers("/posts/**").hasRole(Role.MEMBER.getValue())
+                                .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/members/**").hasRole(Role.MEMBER.name())
                                 .requestMatchers("/**").permitAll()
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(allowedOrigins);
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+        corsConfiguration.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 
     @Bean
